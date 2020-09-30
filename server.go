92c879a7e57cb7a437017a9e2bbfb047af33165a
iminfinity <this.is.me.infinity@gmail.com>
2020-09-30 22:37:06 +0530

    heroku init 4

--- server.go
+++ server.go
@@ -3,11 +3,17 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"os"
 )
 
+func check(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "<h1>Hello</h1>")
+}
+
 func main() {
-	// port := os.Getenv("PORT")
-	http.Handle("/", http.FileServer(http.Dir("./frontend/build/")))
-	fmt.Println("Server running at port 8000")
-	http.ListenAndServe(":8000", nil)
+	port := os.Getenv("PORT")
+	// http.Handle("/", http.FileServer(http.Dir("./frontend/build/")))
+	http.HandleFunc("/", check)
+	fmt.Println("Server running at port " + port)
+	http.ListenAndServe(":"+port, nil)
 }
