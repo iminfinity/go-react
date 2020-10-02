f18216d99dddacee58d37a9879556154727b0b67
iminfinity <this.is.me.infinity@gmail.com>
2020-10-02 23:19:20 +0530

    pwa

--- server.go
+++ server.go
@@ -8,8 +8,8 @@ import (
 )
 
 func main() {
-	// port := os.Getenv("PORT")
-	port := "8000"
+	port := os.Getenv("PORT")
+	// port := "8000"
 	http.HandleFunc("/movies", backend.MovieHandler)
 	http.Handle("/", http.FileServer(http.Dir("./frontend/build/")))
 	fmt.Println("Server running at port " + port)
