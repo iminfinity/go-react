0f7914204123bb3701cc9b53a9e532e5a26e960e
iminfinity <this.is.me.infinity@gmail.com>
2020-10-11 21:10:47 +0530

    serving react and rest api on same port

--- frontend/src/pages/home/home.component.jsx
+++ frontend/src/pages/home/home.component.jsx
@@ -7,7 +7,7 @@ import axios from "axios";
 const Home = () => {
   const [items, setItems] = useState("items");
   useEffect(() => {
-    axios.get("http://localhost:8080/posts/1").then(response => response.data).then(data => console.log(data)).catch(error => console.log(error))
+    axios.get("http://go-react-rest.herokuapp/posts/1").then(response => response.data).then(data => console.log(data)).catch(error => console.log(error))
   }, [])
   return (
     <div>
